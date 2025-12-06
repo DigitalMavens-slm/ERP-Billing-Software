ae5207c763f8dadb7537b5865ea22ccea9c405d0
lakshmanandev3 <lakshmanandev3@gmail.com>
2025-12-06 17:31:56 +0530

    frondent url add backend env

--- Backend/Server.js
+++ Backend/Server.js
@@ -35,12 +35,17 @@ dotenv.config({path:path.join(__dirname,"config/config.env")})
 app.use(express.json());
 app.use(cookieParser());
 
-// app.use(cors({
-//   origin: "http://localhost:5173",
-//   credentials: true
-// }));
 
-app.use(cors());
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://erp-billing-software-10.onrender.com"   // YOUR FRONTEND URL
+  ],
+  credentials: true
+}));
+
+
 
 
 // app.use("/uploads", express.static(path.join(__dirname, "uploads")));
